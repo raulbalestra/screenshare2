c94d298bd447201b688037c6cfd5750ae1c1d822
Higor Felipe Ribeiro <higorfeliperibeiro247@gmail.com>
2024-09-20 10:20:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -181,6 +181,15 @@ def admin_dashboard():
 
 # Criação do banco de dados ao iniciar
 create_database()
+# Error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 # Iniciar o aplicativo com acesso externo
 if __name__ == "__main__":
